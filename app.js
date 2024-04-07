d8b70fa1ce8130ab3869b06712394deee79e6e43
rümeysa <email@example.com>
2024-04-07 04:27:52 +0300

    postman ve api

--- app.js
+++ app.js
@@ -18,6 +18,8 @@ app.set('view engine', 'ejs');
 
 //MIDDLEWARE
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
 
 //ROUTES
 app.use('/', pageRoute);
