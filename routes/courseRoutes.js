7bb20fd33623d010aa2a6da1cfba5a5171bc1420
rümeysa <email@example.com>
2024-04-07 16:51:58 +0300

    teki sayfa ve slugify

--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -4,5 +4,6 @@ const courseController=require('../controllers/courseControllers');
 const router=express.Router();
 router.route('/').post(courseController.createCourse);
 router.route('/').get(courseController.getAllCourses);
+router.route('/:slug').get(courseController.getCourse);
 
 module.exports=router;
