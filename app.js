fd32cad79ab67b8eab917d9ddfcafed8f7417787
rümeysa <email@example.com>
2024-04-07 15:30:15 +0300

    courses page

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ app.set('view engine', 'ejs');
 //MIDDLEWARE
 app.use(express.static('public'));
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({extended:true})); 
 
 //ROUTES
 app.use('/', pageRoute);
--- routes/courseRoutes.js
+++ routes/courseRoutes.js
@@ -3,5 +3,6 @@ const courseController=require('../controllers/courseControllers');
 
 const router=express.Router();
 router.route('/').post(courseController.createCourse);
+router.route('/').get(courseController.getAllCourses);
 
 module.exports=router;
