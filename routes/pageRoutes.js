5207d5ef50ca5e9fa5f59e5a3dcf0d911c972b09
rümeysa <email@example.com>
2024-04-08 02:57:57 +0300

    kayıt sayfası

--- routes/pageRoutes.js
+++ routes/pageRoutes.js
@@ -4,5 +4,6 @@ const pageController=require('../controllers/pageController');
 const router=express.Router();
 router.route('/').get(pageController.getIndexPage);
 router.route('/about').get(pageController.getAboutPage);
+router.route('/register').get(pageController.getRegisterPage);
 
 module.exports=router;
